cdbb4dc5fd0bbc08451830c886ae8b0e8772d6b7
TabiDavid <55420549+tabiDO@users.noreply.github.com>
2020-03-17 20:58:04 -0500

    commit

--- js/weather.js
+++ js/weather.js
@@ -2,28 +2,31 @@ const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&units
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
-    // console.log(jsObject); We don't need this in production version.
-    document.getElementById('currently').textContent = jsObject.weather[0].main; //Current condition description
-    document.getElementById('current-temp').textContent = jsObject.main.temp + " ˚F"; //Current temperature in Fahrenheit
-    document.getElementById('humidity').textContent = jsObject.main.humidity + " %"; //Hum1n1d1ty
-    document.getElementById('wind-speed').textContent = jsObject.wind.speed + " mph"; //W1nd Sp33d
+    console.log(jsObject)
+    document.getElementById('currently').textContent = jsObject.weather[0].main; 
+    document.getElementById('current-temp').textContent = jsObject.main.temp + " ˚F"; 
+    document.getElementById('humidity').textContent = jsObject.main.humidity + " %"; 
+    document.getElementById('wind-speed').textContent = jsObject.wind.speed + " mph"; 
 
     let t = jsObject.main.temp;
     let s = jsObject.wind.speed;
 
     if (t <= 50 && s > 3) {
       let chill = 35.74 + 0.6215 * t - 35.75 * Math.pow(s, .16) + 0.4275 * t * Math.pow(s, .16);
-      document.getElementById("windchill").textContent = chill.toFixed(2) + " ˚F"; //W1nd Ch177
+      document.getElementById("windchill").textContent = chill.toFixed(2) + " ˚F";
     } else {
       document.getElementById("windchill").textContent = "N/A"; //W1nd Ch177
     }
   });
 
+//Forcast// The url and all five days
+
 const apiURL2 = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=aee52151b6cc7c7556030fdb68db7347";
 fetch(apiURL2)
   .then((response) => response.json())
   .then((jsObject2) => {
-    // console.log(jsObject2); We don't need this in production version.
+    
+
     let fivedayforecast = [];
     let day = 1;
     let weekday = [];
@@ -32,49 +35,16 @@ fetch(apiURL2)
     for (let i = 0; i < jsObject2.list.length && day < 6; i++) {
       if (jsObject2.list[i].dt_txt.includes("18:00:00")) {
         fivedayforecast[i] = jsObject2.list[i].main.temp;
-        const imagesrc = 'https://openweathermap.org/img/w/' + jsObject2.list[i].weather[0].icon + '.png'; // n0t3 th3 c0ncat3nat10n
-        const desc = jsObject2.list[i].weather[0].description; // n0t3 h0w w3 r3f3r3nc3 th3 w3ath3r array
+        const imagesrc = 'https://openweathermap.org/img/w/' + jsObject2.list[i].weather[0].icon + '.png';
+        const desc = jsObject2.list[i].weather[0].description; 
 
         let now = new Date(jsObject2.list[i].dt_txt);
         weekday = days[now.getDay()];
         document.getElementById("day" + day).innerHTML = weekday;
         document.getElementById("forecast" + day).innerHTML = fivedayforecast[i] + "&deg; F";
-        document.getElementById("icon" + day).setAttribute("src", imagesrc); // f0cus 0n th3 s3tAttribut3() m3th0d
+        document.getElementById("icon" + day).setAttribute("src", imagesrc); 
         document.getElementById("icon" + day).setAttribute("alt", desc);
         day++;
-      }
-    }
-  });
-
-const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
-
-fetch(requestURL)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (jsonObject) {
-   // console.table(jsonObject); temporary checking for valid response and data parsing. We don't need this in production version.
-    const towns = jsonObject['towns'];
-
-    for (let i = 0; i < towns.length; i++) {
-      if (towns[i].name == "Preston") {
-        // Create HTML elements as variables
-        let eventul = document.createElement('ul');
-        let eventdiv = document.createElement('div');
-        for (let cont = 0; cont < towns[i].events.length; cont++) {
-          // Create HTML elements as variable
-          let list = document.createElement('li');
-          // Class
-          list.setAttribute('class', 'eventlist');
-          //Assign variables content from JSON
-          list.textContent = towns[i].events[cont];
-          //Append HTML items
-          eventul.appendChild(list);
-        }
-        //Append HTML items
-        eventdiv.appendChild(eventul);
-        //Commit to DOM
-        document.querySelector('div.eventdiv').appendChild(eventdiv);
       }
     }
   });
